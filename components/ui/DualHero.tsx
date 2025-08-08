9db678908d67547ca61478025e132005d712e80c
carolinmimmie <carolinmimmie@gmail.com>
2025-08-08 22:57:16 +0200

    dualhero responsiv

--- components/ui/DualHero.tsx
+++ components/ui/DualHero.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 const DualHero = () => {
   return (
-    <div className="relative flex w-full h-[660px]">
-      <div className="relative w-1/2 h-full">
+    <div className="relative flex flex-col sm:flex-row w-full h-[660px]">
+      <div className="relative w-full sm:w-1/2 h-full">
         <Image src="/images/luna.jpg" alt="hero1" fill objectFit="cover" />
       </div>
-      <div className="relative w-1/2 h-full">
+      <div className="relative w-full sm:w-1/2 h-full">
         <Image src="/images/sage.jpg" alt="hero2" fill objectFit="cover" />
       </div>
       <div className="absolute inset-0 flex flex-col items-center justify-end mb-12">
